3a0590f543aa56fb77f6915fc2c6fb63baca73fa
gkddyd <gkddyd@naver.com>
2022-01-25 15:39:13 +0900

    jsx에 html 적용하기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Start React 200!</h1>
+      <p>HTML 적용하기</p>
     </div>
-  );
+  )
 }
 
 export default App;
