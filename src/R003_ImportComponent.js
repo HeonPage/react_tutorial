cabe3fde6c1cc2a6ac86f28302a3daef28a53911
gkddyd <gkddyd@naver.com>
2022-01-28 00:01:46 +0900

    Component 사용하기

---
+++ src/R003_ImportComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class R003_ImportComponent extends Component {
+    state = {}
+    render() {
+        return (
+            <h2>[THIS IS IMPORTED COMPONENT]</h2>
+        );
+    }
+}
+
+export default R003_ImportComponent;
